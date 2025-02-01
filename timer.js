95a8d23c3d7fd3937698b98c8e5034fd1d1fa130
Tanat Piumsuwan (Laptop) <tanat_pi@cmu.ac.th>
2025-02-01 18:15:43 +0700

    Finish Task 13 by using setInterval() function, along with additional choice to stop the program by pressing 'q' on the keyboard.

--- timer.js
+++ timer.js
@@ -1,8 +1,22 @@
-waitAndPrint = (message, delay) => {
-    setTimeout(() => {
-        console.log(message);
+let count = 0;
+
+const waitAndPrint = (message, delay) => {
+    console.log("Press 'q' to stop the function.");
+
+    const interval = setInterval(() => {
+        console.log(message + ' world'.repeat(count));
+        count++;
     }, delay);
+
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', (key) => {
+        if (key.toString() === 'q') {
+            clearInterval(interval);
+            console.log('Stopped.');
+            process.exit();
+        }
+    });
 }
 
-// Example usage:
-waitAndPrint("Hello, world!", 2000); 
+waitAndPrint('hello world', 3000);
