650498d7e884aaee5c3d86b7c1e4c8e20c00d207
Tanat Piumsuwan (Laptop) <tanat_pi@cmu.ac.th>
2025-02-01 16:55:31 +0700

    Finish Task 7

--- for.js
+++ for.js
@@ -2,3 +2,20 @@ let i = 0;
 for (i = 0; i < 10; i++) {
   console.log(i);
 }
+
+let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+for (let i = 0; i < color.length; i++) {
+  console.log(color[i])
+}
+
+let names = ['Poom', 'Film', 'Pim', 'Baitoey', 'Thana']
+for (let i = 0; i < names.length; i++) {
+    if (names[i].length > 6) {
+        console.log('สวัสดีคุณ', names[i])   
+    }
+    else {
+        console.log('Hello', names[i])   
+    }
+    
+}
+
