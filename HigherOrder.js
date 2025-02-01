d7fceb08fa5f00f080eb67e433de86ebe9c0735b
Tanat Piumsuwan (Laptop) <tanat_pi@cmu.ac.th>
2025-02-01 17:49:28 +0700

    Finish Task 12

--- HigherOrder.js
+++ HigherOrder.js
@@ -1,7 +1,15 @@
 const n = [1, 2, 3, 4, 5];
+const m = [12345, 25, 37, 49, 81];
 const square = n.map((num) => num * num);
 console.log(square);
 
 const even = n.filter((num) => num % 2 === 0);
 console.log(even);
 
+const divisibleby3_cube = (list) => {
+    const divisibleby3 = list.filter((num) => num % 3 === 0)
+    return divisibleby3.map((num) => num * num * num)
+}
+
+console.log(divisibleby3_cube(n));
+console.log(divisibleby3_cube(m));
