c1e5d715a377ba05160a9cd958a0b4f0af82cc5e
Tanat Piumsuwan (Laptop) <tanat_pi@cmu.ac.th>
2025-02-01 17:15:35 +0700

    Finish Task 9

--- function.js
+++ function.js
@@ -16,3 +16,15 @@ console.log(sum(test))
 const multiply = (x,y) => { return x * y }
 
 console.log(multiply(5, 10)) 
+
+const greater = (x,y) => { 
+    if (x > y) {
+        return x
+    } else if (y > x){
+        return y
+    } else {
+        return 'x and y are equal at ' + x  
+    }
+}
+
+console.log(greater(11, 10)) 
