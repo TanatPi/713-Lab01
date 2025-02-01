066cc84a22ae9156fa618ffc0bd9468b892c5059
Tanat Piumsuwan (Laptop) <tanat_pi@cmu.ac.th>
2025-02-01 17:26:32 +0700

    Finish Task 11

--- json.js
+++ json.js
@@ -7,3 +7,25 @@ const object = {
 console.log(object.name)
 const jsonStr = JSON.stringify(object);
 console.log(jsonStr)
+
+const studentobject1 = {
+    name: "James",
+    age: 18,
+    GPA: 3.0
+}
+const studentobject2 = {
+    name: "Arthur",
+    age: 19,
+    GPA: 1.5
+}
+
+const GPAcheck = (object) => {
+    if (object.GPA > 2.0) {
+        return JSON.stringify(object);
+    } else {
+        return 'คุณ ' + object.name + ' พ้นสภาพ';
+    }
+}
+
+console.log(GPAcheck(studentobject1))
+console.log(GPAcheck(studentobject2))
