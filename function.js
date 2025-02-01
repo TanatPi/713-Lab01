745f4407d6bda24c0158561e9887797b430bb753
Tanat Piumsuwan (Laptop) <tanat_pi@cmu.ac.th>
2025-02-01 17:06:18 +0700

    Finish Task 8

--- function.js
+++ function.js
@@ -4,3 +4,14 @@ function greet(name){
 
 console.log(greet('John'))
 
+function sum(list){
+    a = 0
+    for (i=0; i < list.length; i++) {
+        a = a + list[i]
+    }
+    return a
+}
+
+test = [10,20,30]
+console.log(sum(test))
+
